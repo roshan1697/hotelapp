8c6926e9afb77feee544774afcb8e74c3ff00160
roshan1697 <roshanpoojary16@gmail.com>
2024-01-10 23:42:28 +0530

    initial hotetapp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <a className="flex-none text-3xl font-semibold " href="#" >Todo App</a>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
